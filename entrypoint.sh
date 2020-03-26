5049b93fb513efadb8fd7e6d7fa3b2ef730977fa
Kaj Rauch <kajrauch@Kajs-MBP.local>
2020-03-26 09:09:10 +0100

    fixed docker flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ echo "$INPUT_SSH_PUBLIC_KEY" >> $HOME/.ssh/known_hosts && chmod 600 $HOME/.ssh/k
 eval $(ssh-agent) && ssh-add $HOME/.ssh/id_rsa
 
 # Connects to host and deploys
-docker --debug-level debug --host "$INPUT_SSH_REMOTE_HOST" "$@"
+docker --log-level debug --host "$INPUT_SSH_REMOTE_HOST" "$@"
